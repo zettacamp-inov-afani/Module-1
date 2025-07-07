4d8b0ba2d0807eb8430470a2827af3dff6e242e2
Inov Afani Eka Putra <inov.putra@zettacamp.pro>
2025-07-07 11:51:18 +0700

    [ZettaCamp][Module 2] Create Task typedef

--- modules/task/task.typedef.js
+++ modules/task/task.typedef.js
@@ -25,4 +25,40 @@ const TaskTypeDefs = gql`
     updated_at: Date!
     deleted_at: Date!
   }
+
+  input CreateTaskInput {
+    test_id: ID!
+    user_id: ID!
+    task_type: EnumTaskType!
+    task_status: EnumTaskStatus!
+    due_date: Date
+  }
+
+  input UpdateTaskInput {
+    test_id: ID!
+    user_id: ID!
+    task_type: EnumTaskType!
+    task_status: EnumTaskStatus!
+    due_date: Date
+  }
+
+  input AssignCorrectorInput {
+    user_id: ID!
+    due_date: Daye
+  }
+
+  type Query {
+  GetOneTask(_id: ID!): Task
+  GetAllTasks: [Task!]!
+  }
+
+  type Mutation {
+  CreateTask(input: CreateTaskInput!): Task!
+  UpdateTask(_id: ID!, inputL UpdateTaskInput!): Task!
+  AssignCorrector(_id: ID!, input: AssignCorrectorInput!): Task!
+  Deletetask(_id: ID!): ID!
+  }
 `;
+
+// *************** EXPORT MODULE ***************
+module.exports = TaskTypeDefs;
