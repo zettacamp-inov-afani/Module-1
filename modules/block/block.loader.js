7ca8187b476820446e2984c0e90f628ec4bef873
Inov Afani Eka Putra <inov.putra@zettacamp.pro>
2025-07-07 16:24:47 +0700

    [ZettaCamp][Module 2] Create BlockLoader

---
+++ modules/block/block.loader.js
@@ -0,0 +1,34 @@
+// *************** IMPORT LIBRARY ***************
+const DataLoader = require('dataloader');
+
+// *************** IMPORT MODULE ***************
+const BlockModel = require('./block.model');
+
+// *************** IMPORT VALIDATOR ***************
+const CommonValidator = require('../../utilities/validator');
+
+function BlockLoader() {
+  // *************** Create new instance of DataLoader
+  return new DataLoader(async (block_id) => {
+    // ***************  Validate the incoming block ids
+    CommonValidator.ValidateMongoObjectIds(block_id);
+
+    // *************** Find all Blocks whose id is in the block ids array
+    const blocks = await BlockModel.find({
+      _id: { $in: block_id },
+    }).lean();
+
+    // *************** Create blockMap object for dictionary
+    const blockMap = {};
+    blocks.forEach((block) => {
+      blockMap[String(block._id)] = block;
+    });
+
+    // *************** Return an array containing subjects in the order of the requested subject ids.
+    const orderedBlocks = block_id.map((_id) => blockMap[String(_id)]);
+    return orderedBlocks;
+  });
+}
+
+// *************** EXPORT MODULE ***************
+module.exports = BlockLoader;
--- modules/subject/subject.loader.js
+++ modules/subject/subject.loader.js
@@ -10,21 +10,21 @@ const CommonValidator = require('../../utilities/validator');
 function SubjectLoader() {
   // *************** Create new instance of DataLoader
   return new DataLoader(async (subject_ids) => {
-    // ***************  Validate the incoming schoolIds
+    // ***************  Validate the incoming subject ids
     CommonValidator.ValidateMongoObjectIds(subject_ids);
 
-    // *************** Find all Schools whose id is in the schoolIds array
+    // *************** Find all Subject whose id is in the subject ids array
     const subjects = await SubjectModel.find({
       _id: { $in: subject_ids },
     }).lean();
 
-    // *************** Create schoolMap object for dictionary
-    const subjecttMap = {};
+    // *************** Create subjectMap object for dictionary
+    const subjectMap = {};
     subjects.forEach((subject) => {
-      subjecttMap[String(subject._id)] = subject;
+      subjectMap[String(subject._id)] = subject;
     });
 
-    // *************** Return an array containing schools in the order of the requested schoolIds.
+    // *************** Return an array containing subjects in the order of the requested subject ids.
     const orderedSubjects = subject_ids.map((_id) => subjectMap[String(_id)]);
     return orderedSubjects;
   });
