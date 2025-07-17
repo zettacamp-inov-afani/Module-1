424e7d0ca433f14e8ef6d074b61eba33d7bafdc4
Inov Afani Eka Putra <inov.putra@zettacamp.pro>
2025-07-17 15:10:06 +0700

    [ZettaCamp][Module 3] Create CalculationResult Model Schema

---
+++ modules/calculationResult/calculation_result.model.js
@@ -0,0 +1,93 @@
+// *************** IMPORT LIBRARY ***************
+const mongoose = require('mongoose');
+
+const CalculationResultSchema = new mongoose.Schema(
+  {
+    // Student reference of CalculationResult
+    student_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'student',
+      required: true,
+    },
+
+    // Overall result of the student
+    overall_result: {
+      type: String,
+      enum: ['PASS', 'FAIL'],
+      required: true,
+    },
+
+    // Detail of the results of CalculationResult
+    results: [
+      {
+        block_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'block',
+          required: true,
+        },
+        block_result: {
+          type: String,
+          enum: ['PASS', 'FAIL'],
+          required: true,
+        },
+        total_mark: {
+          type: Number,
+          required: true,
+        },
+        subject_results: [
+          {
+            subject_id: {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: 'subject',
+              required: true,
+            },
+            subject_result: {
+              type: String,
+              enum: ['PASS', 'FAIL'],
+              required: true,
+            },
+            total_mark: {
+              type: Number,
+              required: true,
+            },
+            test_results: [
+              {
+                test_id: {
+                  type: mongoose.Schema.Types.ObjectId,
+                  ref: 'test',
+                  required: true,
+                },
+                test_result: {
+                  type: String,
+                  enum: ['PASS', 'FAIL'],
+                  required: true,
+                },
+                average_mark: {
+                  type: Number,
+                  required: true,
+                },
+                weighted_mark: {
+                  type: Number,
+                  required: true,
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+
+    // CalculationResult's delete_at detail
+    deleted_at: { type: Date, default: null },
+  },
+  {
+    // CalculationResult's timestamps
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+  }
+);
+
+// *************** EXPORT MODULE ***************
+module.exports = mongoose.model('calculationResult', CalculationResultSchema);
